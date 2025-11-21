0857cfbf6c9a79e032efb78b137a82dd69235c96
BD <oliverluo0723@gmail.com>
2025-11-21 08:11:04 +0800

    feat: 新增 S4 元件，設定 gh-pages 部署

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,6 +3,7 @@ import Header from "@/layouts/Header";
 import Hero from "./components/Hero";
 import S2 from "./components/S2";
 import S3 from "./components/S3";
+import S4 from "./components/S4";
 
 function Home() {
     return (
@@ -13,6 +14,7 @@ function Home() {
             <div className="mt-24">
                 <S3 />
             </div>
+            <S4 />
             <div className="w-full h-svh"></div>
         </div>
     );
---
+++ src/pages/Home/components/S4.tsx
@@ -0,0 +1,9 @@
+function S4() {
+    return (
+        <>
+            <h1>Hello</h1>
+        </>
+    );
+}
+
+export default S4;
